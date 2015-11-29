9aff48fea382aab99338248a5e9b62573232e13d
Mezular <mezular@gmail.com>
2015-11-29 12:33:12 -0500

    Application.java edited
    Added the main JFrame

--- Application.java
+++ Application.java
@@ -4,10 +4,13 @@ Authors: James, Nick, Jon
 
 The main class controlling the overall application.
 */
-
+import javax.swing.JFrame;
 public class Application
 {
 	static void main()
 	{
+		JFrame gameWindow = new JFrame(); //create the game window
+		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
+		gameWindow.setVisible(true);
 	}
 }
