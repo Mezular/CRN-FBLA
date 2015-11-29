30ef223774ad1a26845de772754c359101556b7f
Mezular <mezular@gmail.com>
2015-11-29 13:14:46 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -10,7 +10,9 @@ public class Application
 	static void main()
 	{
 		JFrame gameWindow = new JFrame(); //create the game window
+		GamePanel panel = new JPanel(); //create the game panel
 		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
+		gameWindow.add(panel, BorderLayout.CENTER);
 		gameWindow.setVisible(true);
 	}
 }
