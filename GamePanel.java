ba5a476b464235b467a43d7499095b4946a7ebcd
Mezular <mezular@gmail.com>
2015-11-29 12:36:54 -0500

    GamePanel.java created
    Created the panel used for displaying the graphics

---
+++ GamePanel.java
@@ -0,0 +1,14 @@
+/*
+Cosmic Intruders
+Authors: James, Nick, Jon
+
+The panel displaying the processes of the game
+*/
+
+import javax.swing.JPanel;
+public class GamePanel extends JPanel
+{
+	public GamePanel()
+	{
+	}
+}
