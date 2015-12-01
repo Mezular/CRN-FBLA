9f6c71fb31e57ada84232c870a4f961906516206
Mezular <mezular@gmail.com>
2015-11-30 20:16:34 -0500

    Application.java updated
    title and centering fix

--- Application.java
+++ Application.java
@@ -5,14 +5,18 @@ Authors: James, Nick, Jon
 The main class controlling the overall application.
 */
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 public class Application
 {
-	static void main()
+	public static void main(String[] args)
 	{
 		JFrame gameWindow = new JFrame(); //create the game window
-		GamePanel panel = new JPanel(); //create the game panel
-		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
+		GamePanel panel = new GamePanel(); //create the game panel
+		gameWindow.setSize(1024, 576);
 		gameWindow.add(panel, BorderLayout.CENTER);
+		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
+		gameWindow.setTitle("Cosmic Intruders");
 		gameWindow.setVisible(true);
 	}
 }
