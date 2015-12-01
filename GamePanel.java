4291780621ca7fd149657bf22030c237d470c0b3
Mezular <mezular@gmail.com>
2015-11-30 20:17:13 -0500

    GamePanel.java edited
    added background color and imports

--- GamePanel.java
+++ GamePanel.java
@@ -6,16 +6,19 @@ The panel displaying the processes of the game
 */
 
 import javax.swing.JPanel;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Color;
 public class GamePanel extends JPanel
 {
 	public GamePanel()
 	{
+		setBackground(Color.black);
 	}
 	@Override
     protected void paintComponent(Graphics g) 
     {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.BLACK);
     }
 }
