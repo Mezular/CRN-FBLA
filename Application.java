c77db9c1ec3e815aeb924edb5503bb02d9d7e5a0
Mezular <mezular@gmail.com>
2015-11-30 21:04:13 -0500

    Application.java updated
    Fixed the resource usage

--- Application.java
+++ Application.java
@@ -5,7 +5,9 @@ Authors: James, Nick, Jon
 The main class controlling the overall application.
 */
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 import java.awt.BorderLayout;
+import java.awt.Image;
 public class Application
 {
 	public static void main(String[] args)
@@ -17,6 +19,8 @@ public class Application
 		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
 		gameWindow.setTitle("Cosmic Intruders");
+		Image image = new ImageIcon("Application".getClass().getResource("/cosmiclogo.png")).getImage();
+		gameWindow.setIconImage(image);
 		gameWindow.setVisible(true);
 	}
 }
