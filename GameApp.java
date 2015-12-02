35692dfae32abc209b39fb562ddd9a82eefa4b7f
Mezular <mezular@gmail.com>
2015-12-01 21:56:39 -0500

    GameApp.java updated
    sound fix

--- GameApp.java
+++ GameApp.java
@@ -8,14 +8,11 @@ import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import java.awt.BorderLayout;
 import java.awt.Image;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.application.Application;
 public class GameApp
 {
 	public static void main(String[] args)
 	{
-		Sound danksound = new Sound("dankmusic.wav");
+		Sound danksound = new Sound("/dankmusic.wav");
 		danksound.loop();
 		JFrame gameWindow = new JFrame(); //create the game window
 		GamePanel panel = new GamePanel(); //create the game panel
@@ -24,18 +21,8 @@ public class GameApp
 		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameWindow.setLocationRelativeTo(null); //set the window to appear centered in the screen
 		gameWindow.setTitle("Cosmic Intruders");
-		Image image = new ImageIcon("Application".getClass().getResource("/cosmiclogo.png")).getImage();
+		Image image = new ImageIcon("GameApp".getClass().getResource("/cosmiclogo.png")).getImage();
 		gameWindow.setIconImage(image);
-		//playMusic();
 		gameWindow.setVisible(true);
 	}
-	/*
-	public static void playMusic()
-	{
-		Application.launch();
-		Media bgmusic = new Media("Application".getClass().getResource("/pinballspring.mp3").toString());
-		MediaPlayer player = new MediaPlayer(bgmusic);
-		player.play();
-	}
-	*/
 }
