8659200d57e1839d70b6d044d04c8de9e50ac11c
Mezular <mezular@gmail.com>
2015-12-02 22:06:23 -0500

    Update PlayerShip.java

--- PlayerShip.java
+++ PlayerShip.java
@@ -4,19 +4,24 @@ Authors: James, Nick, Jon
 
 The class representing the player ship
 */
-
+import java.awt.Image;
+import javax.swing.ImageIcon;
 public class PlayerShip
 {
+    private int mid;
     private int hp;
     private int x; //the player's x coordinate
     private int y; //the player's y coordinate
+	private Image sprite;
     private final int l = 96; //the player sprite's length
     private final int h = 108; //the player sprite's height
     public PlayerShip(int windowLength, int windowHeight)
     {
-        x = (windowLength - l)/2;
-	y = (windowHeight - l)/2;
-	hp = 5;
+		sprite = new ImageIcon("GameApp".getClass().getResource("/player.png")).getImage();
+        mid = (windowLength - l)/2;
+		y = windowHeight - 150;
+        x = mid;
+		hp = 10;
     }
     public void setX(int value)
     {
@@ -34,18 +39,12 @@ public class PlayerShip
     {
         return y;
     }
-    public void hit() //when the player gets hit
+    public Image getSprite()
     {
-	hp--;
+        return sprite;
     }
-    public int getL()
+	public void hit() //when the player gets hit
     {
-        return l;
+		hp--;
     }
-    public int getH()
-    {
-        return h;
-    }
-    
 }
-
